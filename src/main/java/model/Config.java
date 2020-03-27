16de9f4b64238c8efdda73dc5d432972d911e785
Nichita Ganja <Nichita.Ganja@endava.com>
2020-03-27 15:52:27 +0200

    @Autowired on setters in @Components

--- src/main/java/model/Config.java
+++ src/main/java/model/Config.java
@@ -8,22 +8,4 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(value = "model")
 public class Config {
 
-    //@Bean
-//    model.Child getChild() {
-//        model.Child child = new model.Child();
-//        child.setMiddle(getMiddle());
-//        return child;
-//    }
-//
-//    @Bean
-//    model.Middle getMiddle(){
-//        model.Middle middle = new model.Middle();
-//        middle.setParent(getParent());
-//        return middle;
-//    }
-//
-//    @Bean
-//    model.Parent getParent() {
-//        return new model.Parent();
-//    }
 }
--- src/main/java/model/Middle.java
+++ src/main/java/model/Middle.java
@@ -1,5 +1,6 @@
 package model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +13,7 @@ public class Middle {
         this.parent = parent;
     }
 
-
+    @Autowired
     public void setParent(Parent parent) {
         this.parent = parent;
     }
